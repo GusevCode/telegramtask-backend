6d564ed2e20b848ce1d69af0ff74e3f60e716edf
GusevCode <gooogle.rostov@gmail.com>
2024-11-03 20:29:53 +0300

    tax fix

--- src/v1/routes/depositRouter.js
+++ src/v1/routes/depositRouter.js
@@ -3,6 +3,7 @@ const depositController = require('../../controllers/depositController');
 
 const router = express.Router();
 
+// in, out
 router.get(':type/:year/:month', depositController.getDeposit);
 router.post(':type', depositController.createDeposit);
 router.patch(':type/:year/:month', depositController.updateDeposit);
--- src/v1/routes/taxRouter.js
+++ src/v1/routes/taxRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const taxController = require('../../controllers/taxController');
 
 const router = express.Router();
 
-router.get(':year/:month', );
-router.post('/', );
-router.patch(':year/:month', );
-router.delete(':year/:month');
+router.get(':year/:month', taxController.getTax);
+router.post('/', taxController.createTax);
+router.patch(':year/:month', taxController.updateTax);
+router.delete(':year/:month', taxController.deleteTax);
 
 module.exports = router;
