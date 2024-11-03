2fd083288f6af794e6428282dff4ea391cc370a2
GusevCode <gooogle.rostov@gmail.com>
2024-11-03 21:14:15 +0300

    depo router

--- src/v1/routes/depositRouter.js
+++ src/v1/routes/depositRouter.js
@@ -4,9 +4,9 @@ const depositController = require('../../controllers/depositController');
 const router = express.Router();
 
 // in, out
-router.get(':type/:year/:month', depositController.getDeposit);
-router.post(':type', depositController.createDeposit);
-router.patch(':type/:year/:month', depositController.updateDeposit);
-router.delete(':type/:year/:month', depositController.deleteDeposit);
+router.get('/:type/:year/:month', depositController.getDeposit);
+router.post('/:type', depositController.createDeposit);
+router.patch('/:type/:year/:month', depositController.updateDeposit);
+router.delete('/:type/:year/:month', depositController.deleteDeposit);
 
 module.exports = router;
