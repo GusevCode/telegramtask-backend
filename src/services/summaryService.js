c8029900fc301edd479c519aec1ee9606f2de98c
GusevCode <gooogle.rostov@gmail.com>
2024-11-09 03:21:41 +0300

    test

--- src/services/summaryService.js
+++ src/services/summaryService.js
@@ -8,34 +8,30 @@ const getTableDataByYear = async (year) => {
     for (let month = 1; month <= 12; month++) {
         earningsData = (await earningService.getTableDataByYearAndMonth(year, `${month}`));
 
-        if (earningsData) {
-            result[month] = {
-                month: month,
-                turnover: earningsData.totalReport.incomeSum, // Оборот
-                profit: earningsData.totalReport.profitSum, // прибыль
-                marginality: 0, // маржинальность
-                amountOfEvents: earningsData.totalReport.amountOfEvents, // Колличество ивентов
-                amountOfHumanActivities: earningsData.totalReport.humanActivities, // ЧМ
-                amountOfNewHumanActivities: earningsData.totalReport.amountOfNewClients, // ЧМ новые
-                averageCheck: 0, // Средний чек
-                ch: 0, //ch
-                promotion: earningsData.totalReport.promotionExpensesSum, // Продвижение
-                org: earningsData.totalReport.orgExpensesSum, // Орграсходы
-                netIncome: 0, // Чистыми
-                investitions: earningsData.totalReport.investitionExpensesSum, // Инвестиции
-                tax: earningsData.totalReport.tax,     // Налоги
-                total: 0, // Деньги в кассе
-            };
+        result[month] = {
+            month: month,
+            turnover: earningsData.totalReport.incomeSum, // Оборот
+            profit: earningsData.totalReport.profitSum, // прибыль
+            marginality: 0, // маржинальность
+            amountOfEvents: earningsData.totalReport.amountOfEvents, // Колличество ивентов
+            amountOfHumanActivities: earningsData.totalReport.humanActivities, // ЧМ
+            amountOfNewHumanActivities: earningsData.totalReport.amountOfNewClients, // ЧМ новые
+            averageCheck: 0, // Средний чек
+            ch: 0, //ch
+            promotion: earningsData.totalReport.promotionExpensesSum, // Продвижение
+            org: earningsData.totalReport.orgExpensesSum, // Орграсходы
+            netIncome: 0, // Чистыми
+            investitions: earningsData.totalReport.investitionExpensesSum, // Инвестиции
+            tax: earningsData.totalReport.tax,     // Налоги
+            total: 0, // Деньги в кассе
+        };
 
-            result[month].marginality = result[month].profit / result[month].turnover;
-            result[month].averageCheck = result[month].turnover / result[month].amountOfHumanActivities;
-            result[month].ch = result[month].profit / result[month].amountOfHumanActivities;
-            result[month].netIncome = result[month].profit - result[month].promotion - result[month].org;
-            result[month].total = result[month].netIncome - result[month].investitions - result[month].tax;
-        } else {
-            result[month] = null;
-        }        
-
+        result[month].marginality = result[month].profit / result[month].turnover;
+        result[month].averageCheck = result[month].turnover / result[month].amountOfHumanActivities;
+        result[month].ch = result[month].profit / result[month].amountOfHumanActivities;
+        result[month].netIncome = result[month].profit - result[month].promotion - result[month].org;
+        result[month].total = result[month].netIncome - result[month].investitions - result[month].tax;
+    
     }
     return result;
 };
